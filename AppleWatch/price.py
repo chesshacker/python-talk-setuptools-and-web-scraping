2ba585f70f44d5a62251d8213d484896d6a7ed57
Steve Ortiz <steve.ortiz@rackspace.com>
2015-07-15 12:06:58 -0500

    add living dangerously comment

--- AppleWatch/price.py
+++ AppleWatch/price.py
@@ -5,6 +5,7 @@ def main():
     url = 'http://store.apple.com/us/buy-watch/apple-watch-sport/38mm-silver-aluminum-case-white-sport-band' \
         '?product=MJ2T2LL/A&step=detail'
     page = requests.get(url)
+    # living dangerously: not checking for errors at any point in this code
     tree = html.fromstring(page.text)
     price = tree.xpath('//span[@itemprop="price"]/text()')
     print price[0].strip()
