9be709bced5c6ed965d9c7ddb8d58976e76e3685
Steve Ortiz <steve.ortiz@rackspace.com>
2015-07-15 12:05:22 -0500

    split long url over two lines

--- AppleWatch/price.py
+++ AppleWatch/price.py
@@ -2,7 +2,9 @@ import requests
 from lxml import html
 
 def main():
-    page = requests.get('http://store.apple.com/us/buy-watch/apple-watch-sport/38mm-silver-aluminum-case-white-sport-band?product=MJ2T2LL/A&step=detail')
+    url = 'http://store.apple.com/us/buy-watch/apple-watch-sport/38mm-silver-aluminum-case-white-sport-band' \
+        '?product=MJ2T2LL/A&step=detail'
+    page = requests.get(url)
     tree = html.fromstring(page.text)
     price = tree.xpath('//span[@itemprop="price"]/text()')
     print price[0].strip()
